c165a56ab9a3e63c66309895b559b6b58f8b9243
pillar star <pillar0124@email.com>
2022-09-26 17:17:43 +0900

    initial commit

---
+++ ui2_pro.py
@@ -0,0 +1,56 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QDialogButtonBox, QVBoxLayout, QLabel
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from ui3_pro import Ui_FaceSwap
+from functools import partial
+
+class Ui_SelectStyle(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.widget = QtWidgets.QWidget()
+        self.widget.setGeometry(QtCore.QRect(40, 60, 400, 400))
+        self.widget.setObjectName("widget")
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        self.gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.gridLayout.setObjectName("gridLayout")
+        
+        self.label_1 = QtWidgets.QLabel(self.widget)
+        self.label_1.setText("")
+        self.label_1.setPixmap(QtGui.QPixmap("photo/style/1.bmp"))
+        self.label_1.setScaledContents(True)
+        self.label_1.setObjectName("label_1")
+        self.label_1.mousePressEvent = partial(self.changeFaceDialog, 1)
+        self.gridLayout.addWidget(self.label_1, 0, 0, 1, 1)
+
+        self.label_2 = QtWidgets.QLabel(self.widget)
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("photo/style/2.bmp"))
+        self.label_2.setScaledContents(True)
+        self.label_2.setObjectName("label_2")
+        self.label_2.mousePressEvent = partial(self.changeFaceDialog, 2)
+        self.gridLayout.addWidget(self.label_2, 0, 2, 1, 1)
+
+        self.label_3 = QtWidgets.QLabel(self.widget)
+        self.label_3.setText("")
+        self.label_3.setPixmap(QtGui.QPixmap("photo/style/3.bmp"))
+        self.label_3.setScaledContents(True)
+        self.label_3.setObjectName("label_3")
+        self.label_3.mousePressEvent = partial(self.changeFaceDialog, 3)
+        self.gridLayout.addWidget(self.label_3, 1, 0, 1, 1)
+
+        self.label_4 = QtWidgets.QLabel(self.widget)
+        self.label_4.setText("")
+        self.label_4.setPixmap(QtGui.QPixmap("photo/style/4.bmp"))
+        self.label_4.setScaledContents(True)
+        self.label_4.setObjectName("label_4")
+        self.label_4.mousePressEvent = partial(self.changeFaceDialog, 4)
+        self.gridLayout.addWidget(self.label_4, 1, 2, 1, 1)
+        self.setLayout(self.gridLayout)
+
+    def changeFaceDialog(self, var, Id):
+        print("Success!")
+        dlg = Ui_FaceSwap(var)
+        dlg.exec()
---
+++ ui3_pro.py
@@ -0,0 +1,83 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QPushButton, QDialogButtonBox, QVBoxLayout, QLabel
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import os
+import cv2
+import argparse
+
+from face_detection import select_face, select_all_faces
+from face_swap import face_swap
+
+class Ui_FaceSwap(QDialog):
+    
+    def __init__(self, Id):
+        super().__init__()
+        src_img = 'photo/1.png'
+        dst_img = 'photo/style/real/' + str(Id) + '.jpg'
+        out_img = 'photo/out.jpg'
+
+        result = self.faceSwap(src_img, dst_img, out_img)
+        self.widget = QtWidgets.QWidget()
+        self.widget.setGeometry(QtCore.QRect(40, 60, 400, 400))
+        self.gridLayout = QtWidgets.QGridLayout(self.widget)
+        
+        self.label_1 = QtWidgets.QLabel(self.widget)
+        self.label_1.setText("")
+        self.label_1.setPixmap(QtGui.QPixmap('photo/out.jpg'))
+        self.label_1.setScaledContents(True)
+        self.label_1.setObjectName("label_1")
+        self.gridLayout.addWidget(self.label_1, 0, 0, 1, 1)
+        self.setLayout(self.gridLayout)
+
+    def faceSwap(self, src_img, dst_img, out_img):
+        src_img = cv2.imread(src_img)
+        dst_img = cv2.imread(dst_img)
+
+        args = {
+            'out' : out_img,
+            'correct_color' : True,
+            'warp_2d' : False,
+            'no_debug_window': True
+        }
+
+        class dotdict(dict):
+            """dot.notation access to dictionary attributes"""
+            __getattr__ = dict.get
+            __setattr__ = dict.__setitem__
+            __delattr__ = dict.__delitem__
+
+        args = dotdict(args)
+        
+        # Select src face
+        src_points, src_shape, src_face = select_face(src_img)
+        # Select dst face
+        dst_faceBoxes = select_all_faces(dst_img)
+
+        if dst_faceBoxes is None:
+            print('Detect 0 Face !!!')
+            exit(-1)
+
+        output = dst_img
+        for k, dst_face in dst_faceBoxes.items():
+            output = face_swap(src_face, dst_face["face"], src_points,
+                            dst_face["points"], dst_face["shape"],
+                            output, args)
+
+        dir_path = os.path.dirname(args.out)
+        if not os.path.isdir(dir_path):
+            os.makedirs(dir_path)
+
+        cv2.imwrite(args.out, output)
+
+        ##For debug
+        if not args.no_debug_window:
+            cv2.imshow("From", dst_img)
+            cv2.imshow("To", output)
+            cv2.waitKey(0)
+            
+            cv2.destroyAllWindows()
+
+        return output
